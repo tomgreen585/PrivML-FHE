4db05cd52cb01319849b11e416dd7c2e4acd7e80
Tom Green <105849298+tomgreen585@users.noreply.github.com>
2025-08-09 02:42:08 +1200

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ app.use(express.static(path.join(__dirname, 'src', 'web_application', 'public'))
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'start.html'));
+  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'start', 'start.html'));
 });
 
 app.get('/border_ml', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'border.html'));
+  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'border_ml', 'border.html'));
 });
 
 app.get('/mnist_ml', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'mnist.html'));
+  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'mnist_ml', 'mnist.html'));
 });
 
 app.get('/face_ml', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'face.html'));
+  res.sendFile(path.join(__dirname, 'src', 'web_application', 'public', 'face_ml', 'face.html'));
 });
 
 // Start server
